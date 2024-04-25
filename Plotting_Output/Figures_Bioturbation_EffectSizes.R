9d651f1da35f1b7636fcb092b5f39577ac4bc77b
Dr. Alison Cribb <86627266+atcribb@users.noreply.github.com>
2024-04-25 11:59:43 +0100

    Update Figures_Bioturbation_EffectSizes.R

--- Plotting_Output/Figures_Bioturbation_EffectSizes.R
+++ Plotting_Output/Figures_Bioturbation_EffectSizes.R
@@ -20,7 +20,7 @@ library(tidyr)
 library(RColorBrewer)
 
 #==== load data ====#
-load('effectsizes_bioturbation_occsub.RData')
+load('Output/effectsizes_bioturbation_occsub.RData')
 results_df <- bioturbation_occsub_results_df
 
 results_df$stage <- factor(results_df$stage, levels=stage_names)
